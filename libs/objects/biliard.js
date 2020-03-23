299ece728efa6b48064bd7a62e3d8b43886ef64b
Ficsor Tamás <ficstamas@gmail.com>
2020-03-23 18:17:39 +0100

    ThreeBSP test usage

--- libs/objects/biliard.js
+++ libs/objects/biliard.js
@@ -1,10 +1,25 @@
 function biliard(){
-    sphere = createMesh(new THREE.SphereGeometry(1, 10, 10));
-    sphere.position.x = -3
-    cube = createMesh(new THREE.BoxGeometry(2, 2, 2));
+    let base = createMesh(new THREE.BoxGeometry(4, 2, 2));
+    let base_bsp = new ThreeBSP(base);
+
+    // Bottom cutting pieces
+    let bottom_cut_1 = createMesh(new THREE.BoxGeometry(4, 1.5, 1.5));
+    bottom_cut_1.position.y = -0.25;
+    let bottom_cut_2 = createMesh(new THREE.BoxGeometry(3.5, 1.5, 2));
+    bottom_cut_2.position.y = -0.25;
+
+    let bottom_cut_1_bsp = ThreeBSP(bottom_cut_1);
+    let bottom_cut_2_bsp = ThreeBSP(bottom_cut_2);
+
+    // Cutting bottom of the table
+    let result_bsp = base.subtract(bottom_cut_1_bsp);
+    result_bsp = subtract.subtract(bottom_cut_2_bsp);
+
+    // Grouping objects
     let group = new THREE.Group();
-    group.add(sphere);
-    group.add(cube);
-    group.position.x = 1.5
+    let final = result_bsp.toMesh()
+    //final.geometry.computeFaceNormals();
+    //final.geometry.computeVertexNormals();
+    group.add(final);
     return group;
 }
